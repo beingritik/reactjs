4e57fa684e7ba4454a4e6f1dafe6f9c9d483afa0
=beingritik <=ritikjain98.r@gmail.com>
2023-12-27 09:25:57 +0530

    without context - handle static data ops

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import "./App.css";
 import React, { useEffect, useState } from "react";
+import "./App.css";
 import Users from "./components/Users";
 import Layout from "./Layout";
 import Home from "./Home";
 import useFetchdata from  "./hooks/useFetchdata";
-
 import {
   Route,
   createBrowserRouter,
@@ -16,21 +15,21 @@ import Singleuser from "./components/Singleuser";
 function App() {
   const [userlist, setUserlist] = useState([]);
   let url = import.meta.env.VITE_URL;
-
+ 
   //using custom hooks for the userlist fucntion
   const userData = useFetchdata(url) ;
-  console.log("userdata==", userData);
-
+  const staticUsers = userData.map(({title,completed,id})=>({title,completed,id}));
   useEffect(() => {
     // Update the userlist state when userData changes
     setUserlist(userData);
   }, [userData]);
 
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
-        <Route path="users" element={<Users users={userlist} />} />
+        <Route path="users" element={<Users users = {userlist} staticdata = {staticUsers}/>} />
         <Route path="user/:id" element={<Singleuser />} />
       </Route>
     )
@@ -40,13 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-  // const fetchUserlist = () => {
-  //   axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-  //     setUserlist(res.data);
-  //   });
-  // };
-  // useEffect(() => {
-  //   fetchUserlist();
-  // }, []);
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
-
-function Users({ users }) {
-
+import Edit from "./Edit";
+import { useState, useEffect } from "react";
+function Users({ users, staticdata }) {
   //   const fetchUsers = () => {
   //     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
   //       setUsers(res.data);
@@ -11,33 +11,76 @@ function Users({ users }) {
   //     fetchUsers();
   //   }, []);
 
+  useEffect(() => {
+    setStaticUsers(staticdata)
+  }, [staticdata]);
+
+  const [staticUsers, setStaticUsers] = useState(staticdata);
+  // const [edit,setedit]= useState(null);
+  const [toEdit, settoEdit] = useState("");
+  const [isEditable, setisEditable] = useState(false);
   const navigate = useNavigate();
-  const viewItem = (id)=>{
+  const viewItem = (id) => {
     navigate(`/user/${id}`);
-  }
+  };
+  const editItem = (user) => {
+    setisEditable(true);
+    settoEdit(user);
+  };
 
+  const finaleditHandler = (data) => {
+    const newArray = staticdata.map((prev) =>
+    prev.id === data.id ? data : prev
+    );
+    setStaticUsers(newArray);
+    // settoEdit(null);
+  };
   return (
-    <div>
-      <table>
-        The users are :
-        <tr>
-          <th> title</th>
-          <th> userId</th>
-          <th> Flag</th>
-          <th>Actions </th>
-        </tr>
-        {users &&
-          users.map((user) => (
-            <tr key={user.id}>
-              <th>{user.title || "not available"}</th>
-              <th>{user.userId || "not fetching .."}</th>
-              <th>{JSON.stringify(user.completed ) || "not having"}</th>
-              <th><button onClick={()=>viewItem(user.id)}> View </button></th>
+    <>
+      <div>
 
-            </tr>
+
+        {isEditable ? <Edit user={toEdit} sendData={finaleditHandler} /> : null}
+
+
+        the static are:
+        {staticUsers &&
+          staticUsers.map((staticUser) => (
+            <ol key={staticUser.id}>
+              <li>{staticUser.title} </li>
+              <li>{JSON.stringify(staticUser.completed)} </li>
+              <button onClick={() => editItem(staticUser)}> Edit </button>
+            </ol>
           ))}
-      </table>
-    </div>
+
+
+
+        {/* The users are : */}
+        {/* <table>
+          <tr>
+            <th> title</th>
+            <th> userId</th>
+            <th> Flag</th>
+            <th>Actions </th>
+          </tr>
+          {users &&
+            users.map((user) => (
+              <tr key={user.id}>
+                <th>{user.title || "not available"}</th>
+                <th>{user.userId || "not fetching .."}</th>
+                <th>{JSON.stringify(user.completed) || "not having"}</th>
+                <th>
+                  <button onClick={() => viewItem(user.id)}> View </button>
+                </th>
+                <th>
+                  <button onClick={() => editItem(user)}> Edit </button>
+                </th>
+              </tr>
+            ))}
+        </table> */}
+        
+      </div>
+    </>
   );
 }
 
